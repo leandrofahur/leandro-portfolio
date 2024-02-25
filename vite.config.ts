db666a575813a949b16c2cd3b14b5d14cfad488f
Leandro Miranda Fahur Machado <fahurleandro@gmail.com>
2024-02-24 16:00:39 -0800

    portfolio project

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    host: "0.0.0.0",
-    hmr: {
-      // Adjust if necessary, the default port is 24678
-      port: 24678,
-    },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+})
